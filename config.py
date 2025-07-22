f1b8bdf54c969500edc07d347303f30b4cf3faf8
TgkCapture <tawongakanyenda5@gmail.com>
2025-07-22 11:39:59 +0200

    feat: configs

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-123'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'welfare.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+    REPORT_FOLDER = os.path.join(basedir, 'reports')
+    
+    # Google Sheets
+    GSHEETS_CREDENTIALS = os.path.join(basedir, 'credentials.json')
+    GSHEETS_SHEET_ID = os.environ.get('GSHEETS_SHEET_ID')
