d1944b62566675c2bad6695f1cb601a9d4614903
TgkCapture <tawongakanyenda5@gmail.com>
2025-12-17 12:19:29 +0200

    feat: updated

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 # run.py
+import os
 from app import create_app
+from app.config import config
 
-app = create_app()
+app = create_app(config[os.getenv('FLASK_ENV') or 'default'])
 
 if __name__ == '__main__':
     app.run(debug=True)
