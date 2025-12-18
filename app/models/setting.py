76f740a53202ed3c0961d2b7c86be4ba4ea04329
TgkCapture <tawongakanyenda5@gmail.com>
2025-12-18 17:15:04 +0200

    fix: db errors

--- app/models/setting.py
+++ app/models/setting.py
@@ -1,45 +1,30 @@
 # app/models/setting.py
+from app.extensions import db
 
-class Setting:
-    """Base Setting class"""
+class Setting(db.Model):
+    __tablename__ = 'settings'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(100), unique=True, nullable=False)
+    value = db.Column(db.Text, nullable=True)
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), 
+                          onupdate=db.func.current_timestamp())
     
     @classmethod
     def get_value(cls, key, default=None):
-        # This will be implemented after db is available
-        pass
+        setting = cls.query.filter_by(key=key).first()
+        return setting.value if setting else default
     
     @classmethod
     def set_value(cls, key, value):
-        # This will be implemented after db is available
-        pass
-
-def get_setting_model(db):
-    class SettingModel(Setting, db.Model):
-        __tablename__ = 'settings'
-        
-        id = db.Column(db.Integer, primary_key=True)
-        key = db.Column(db.String(100), unique=True, nullable=False)
-        value = db.Column(db.Text, nullable=True)
-        updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), 
-                              onupdate=db.func.current_timestamp())
-        
-        @classmethod
-        def get_value(cls, key, default=None):
-            setting = cls.query.filter_by(key=key).first()
-            return setting.value if setting else default
-        
-        @classmethod
-        def set_value(cls, key, value):
-            setting = cls.query.filter_by(key=key).first()
-            if setting:
-                setting.value = value
-            else:
-                setting = cls(key=key, value=value)
-                db.session.add(setting)
-            db.session.commit()
-            return setting
-        
-        def __repr__(self):
-            return f'<Setting {self.key}>'
+        setting = cls.query.filter_by(key=key).first()
+        if setting:
+            setting.value = value
+        else:
+            setting = cls(key=key, value=value)
+            db.session.add(setting)
+        db.session.commit()
+        return setting
     
-    return SettingModel
+    def __repr__(self):
+        return f'<Setting {self.key}>'
--- app/models/user.py
+++ app/models/user.py
@@ -1,22 +1,15 @@
 # app/models/user.py
 from flask_login import UserMixin
-# Don't import db here
+from app.extensions import db
 
-class User(UserMixin):
-    """Base User class without SQLAlchemy"""
-    pass
-
-def get_user_model(db):
-    class UserModel(User, db.Model):
-        __tablename__ = 'users'
-        
-        id = db.Column(db.Integer, primary_key=True)
-        email = db.Column(db.String(100), unique=True, nullable=False)
-        password = db.Column(db.String(100), nullable=False)
-        is_active = db.Column(db.Boolean, default=True)
-        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-        
-        def __repr__(self):
-            return f'<User {self.email}>'
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     
-    return UserModel
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    
+    def __repr__(self):
+        return f'<User {self.email}>'
